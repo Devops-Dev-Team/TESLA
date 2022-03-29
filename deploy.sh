6103153b709e6a47cf9a672c5c72b0c344386e50
zafar siddiqui <zafar siddiqui706@gmail.com>
2022-03-29 22:12:38 +0530

    second git commit

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 this is a deployment based architecture.
 please use only specific deployments.
 mandatory things to be deployed first.
+adding some more comments to verify the process
+please have a clear set of vision & mindset
 
 
 thank you
