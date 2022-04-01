2f2d541250030ce92347901a51f70ecfc35b52af
Jaarvis123 <93350139+Jaarvis123@users.noreply.github.com>
2022-04-01 15:55:14 +0530

    Gitub addition

--- stringutils.java
+++ stringutils.java
@@ -1,3 +1,7 @@
+adding some codes from Github  to verify in GIT.
+ signing off
+ roger
+ over n out
 updating some content for git push & pull
 this is user & string based varaible executable file.
 please include all the mandatory string.
